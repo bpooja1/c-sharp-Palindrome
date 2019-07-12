22e19558018d1e5ce1c7c3b2655a1b0293eae987
bpooja1 <52168502+bpooja1@users.noreply.github.com>
2019-07-12 23:49:13 +0545

    Add files via upload

---
+++ Palindrome.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Test
+    {
+        int num;
+        int rev;
+        int mod= 0;
+        int temp;
+        public void Palindrome()
+        {
+            Console.WriteLine("Enter a number");
+            num = int.Parse(Console.ReadLine());
+            temp = num;
+            while(num>0)
+            {
+                rev = num % 10;
+                mod = (mod * 10) + rev;
+                num = num / 10;
+            }
+            if(temp==mod)
+            {
+                Console.WriteLine("The given number is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("The given number is Not Palindrome");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Test t = new Test();
+            t.Palindrome();
+        }
+    }
+}
